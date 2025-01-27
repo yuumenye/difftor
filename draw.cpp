d0b6e1cce3cbd7ed84c36ae7127898623c7f80e1
yuumenye <yumenye@proton.me>
2025-01-27 21:35:14 +0300

    Add value types

--- draw.cpp
+++ draw.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "tree.h"
 #include "draw.h"
 
@@ -8,6 +9,9 @@ static void compile_dot(char *filename);
 static void write_all(FILE *file, struct node *node);
 static void write_params(FILE *file);
 static void write_nodes(FILE *file, struct node *node);
+static void write_num(FILE *file, struct node *node);
+static void write_var(FILE *file, struct node *node);
+static void write_op(FILE *file, struct node *node);
 static void write_edges(FILE *file, struct node *node);
 
 /* draw tree using graphviz */
@@ -51,6 +55,7 @@ static void write_params(FILE *file)
 {
         fprintf(file, "  node [\n");
         fprintf(file, "    shape = \"circle\";\n");
+        fprintf(file, "    style = \"filled\";\n");
         fprintf(file, "  ]\n");
         fprintf(file, "  edge [label = \" \"];\n");
 }
@@ -60,9 +65,54 @@ static void write_nodes(FILE *file, struct node *node)
         if (!node)
                 return;
 
-        fprintf(file, "  n%p [label = \"%d\"];\n", node, node->val);
+        switch (node->type) {
+                case NUM:
+                        write_num(file, node);
+                        break;
+                case VAR:
+                        write_var(file, node);
+                        break;
+                case OP:
+                        write_op(file, node);
+                        break;
+                default:
+                        fprintf(stderr, "error: unknown value type\n");
+                        exit(1);
+        }
+
         write_nodes(file, node->left);
         write_nodes(file, node->right);
+}
+
+static void write_num(FILE *file, struct node *node)
+{
+        fprintf(file, "  n%p [label = \"%d\", fillcolor = \"#FFF8DE\"];\n",
+                        node, node->val);
+}
+
+static void write_var(FILE *file, struct node *node)
+{
+        fprintf(file, "  n%p [label = \"%c\", fillcolor = \"#C5D3E8\"];\n",
+                        node, node->val);
+}
+
+static void write_op(FILE *file, struct node *node)
+{
+        struct op_desc operations[] = {ADD, "+", SUB, "-", MUL, "*", DIV, "/",
+                POW, "^"};
+        size_t n_operations = sizeof(operations)/sizeof(operations[0]);
+
+        const int namelen = 5;
+        char name[namelen] = "";
+
+        for (size_t i = 0; i < n_operations; ++i)
+                if (node->val == operations[i].val) {
+                        strncpy(name, operations[i].name, namelen);
+                        break;
+                }
+
+        fprintf(file, "  n%p [label = \"%s\", fillcolor = \"#D0E8C5\"];\n",
+                        node, name);
 }
 
 static void write_edges(FILE *file, struct node *node)
--- tree.h
+++ tree.h
@@ -1,7 +1,27 @@
 #ifndef TREE_H
 #define TREE_H
 
+enum op_type {
+        ADD,
+        SUB,
+        MUL,
+        DIV,
+        POW
+};
+
+enum val_type {
+        NUM,
+        VAR,
+        OP
+};
+
+struct op_desc {
+        enum op_type val;
+        const char *name;
+};
+
 struct node {
+        enum val_type type;
         int val;
         struct node *left;
         struct node *right;
